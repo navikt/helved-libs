d4cf8aefed24c63aac8da1ca02d71b8964b86355
Robin <robin.tordly@nav.no>
2024-05-16 11:24:51 +0200

    jaxb-api is always required when using xml-mapper

--- libs/xml/build.gradle.kts
+++ libs/xml/build.gradle.kts
@@ -3,6 +3,7 @@ val ktorVersion = "2.3.9"
 dependencies {
     api("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.17.1")
     api("javax.xml.bind:jaxb-api:2.3.1")
+    api("org.glassfish.jaxb:jaxb-runtime:2.3.2")
 
     testImplementation(kotlin("test"))
     testImplementation("org.glassfish.jaxb:jaxb-runtime:2.3.2")
