f3f0eaddeaf789cdb6f8217064bb95b0d44c4d53
Robin <robin.tordly@nav.no>
2024-06-18 09:03:06 +0200

    wrap action in lock, similar to how a mutex is impl

--- libs/postgres/main/libs/postgres/concurrency/Lock.kt
+++ libs/postgres/main/libs/postgres/concurrency/Lock.kt
@@ -1,5 +1,9 @@
 package libs.postgres.concurrency
 
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
+
 suspend fun lock(on: String): Boolean = transaction {
     val query = "SELECT PG_ADVISORY_LOCK(${on.hashCode()})"
     coroutineContext.connection.prepareStatement(query).use { stmt ->
@@ -18,5 +22,18 @@ suspend fun unlock(on: String): Boolean = transaction {
     val query = "SELECT PG_ADVISORY_UNLOCK(${on.hashCode()})"
     coroutineContext.connection.prepareStatement(query).use { stmt ->
         stmt.execute()
+    }
+}
+
+@OptIn(ExperimentalContracts::class)
+suspend inline fun <T> withLock(owner: String, action: () -> T): T {
+    contract {
+        callsInPlace(action, InvocationKind.EXACTLY_ONCE)
+    }
+    lock(owner)
+    return try {
+        action()
+    } finally {
+        unlock(owner)
     }
 }
