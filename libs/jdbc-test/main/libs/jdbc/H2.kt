8e082d499ed9c9c9cb87a55a7880fa5625fa82b2
Robin <robin.tordly@nav.no>
2024-10-09 15:48:24 +0200

    H2 trenger bare en config enn så lenge

--- libs/jdbc-test/main/libs/jdbc/H2.kt
+++ libs/jdbc-test/main/libs/jdbc/H2.kt
@@ -1,11 +1,6 @@
 package libs.jdbc
 
-import kotlinx.coroutines.withContext
 import libs.postgres.JdbcConfig
-import libs.postgres.Postgres
-import libs.postgres.concurrency.connection
-import libs.postgres.concurrency.transaction
-import libs.utils.appLog
 
 object H2 {
     val config by lazy {
@@ -18,15 +13,5 @@ object H2 {
             url = "jdbc:h2:mem:test_db;MODE=PostgreSQL",
             driver = "org.h2.Driver",
         )
-    }
-
-    suspend fun clear(table: String) = withContext(Postgres.context) {
-        transaction {
-            coroutineContext.connection.prepareStatement("SET REFERENTIAL_INTEGRITY FALSE").execute()
-            coroutineContext.connection.prepareStatement("TRUNCATE TABLE $table").execute()
-            coroutineContext.connection.prepareStatement("SET REFERENTIAL_INTEGRITY TRUE").execute()
-        }.also {
-            appLog.info("table '$table' trunctated.")
-        }
     }
 }
