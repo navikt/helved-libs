4a5fd04363b492d53f634036d2c48bb92356326c
Robin <robin.tordly@nav.no>
2025-03-05 21:24:43 +0100

    save produced keys to prevent stackoverflow

--- libs/kafka-test/main/libs/kafka/MockProducer.kt
+++ libs/kafka-test/main/libs/kafka/MockProducer.kt
@@ -12,9 +12,15 @@ class MockProducer<K : Any, V : Any>(
     topic: Topic<K, V>,
     private val testTopic: TestTopic<K, V>,
 ) : ApacheMockProducer<K, V>(true, topic.serdes.key.serializer(), topic.serdes.value.serializer()) {
+    private val producedKeys = mutableSetOf<K>()
+
     override fun send(record: ProducerRecord<K, V>, callback: Callback): Future<RecordMetadata> {
-        testTopic.produce(record.key(), record::value)
         val metadata = RecordMetadata(TopicPartition(record.topic(), 0), 0L, 0, System.currentTimeMillis(), 0, 0)
+        if (producedKeys.contains(record.key())) {
+            return CompletableFuture.completedFuture(metadata)
+        }
+        producedKeys.add(record.key())
+        testTopic.produce(record.key(), record::value)
         callback.onCompletion(metadata, null)
         return CompletableFuture.completedFuture(metadata)
     }
