efd1a520fbdf95f7fe6d5a1b05d6569f80d5c0a2
JorgenNyborgChristensen <jony@computas.com>
2025-03-05 10:20:20 +0100

    Flytt MQFake til mq-test
    Co-authored-by: stephen ramthun <stephen.ramthun@nav.no>
    Co-authored-by: anders skifte <anders.skifte@nav.no>
    Co-authored-by: robin tordly <robin.tordly@nav.no>

--- libs/mq-test/build.gradle.kts
+++ libs/mq-test/build.gradle.kts
@@ -2,4 +2,6 @@ dependencies {
     implementation(project(":libs:mq"))
     implementation(project(":libs:utils"))
     api("org.testcontainers:testcontainers:1.20.5")
+    api("com.ibm.mq:com.ibm.mq.allclient:9.4.1.0")
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
 }
