ffbe661c7ec1988980e7059503adaee084c71c6e
robin <robin.tordly@gmail.com>
2025-07-01 14:44:31 +0200

    tracing mq og mq-test er flyttet til helved-utbetaling
    Co-authored-by: jørgen nyborg-christensen <jorgen.nyborg-christensen@nav.no>
    Co-authored-by: trond inge lillesand <trond.inge.lillesand@nav.no>

--- libs/mq-test/build.gradle.kts
+++
@@ -1,7 +0,0 @@
-dependencies {
-    implementation(project(":libs:mq"))
-    implementation(project(":libs:utils"))
-    api("org.testcontainers:testcontainers:1.21.3")
-    api("com.ibm.mq:com.ibm.mq.allclient:9.4.1.0")
-    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
-}
